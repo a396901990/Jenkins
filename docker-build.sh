c301db7ea59957b53cf64444d2ef4e4927d31b50
Dean Guo <396901990@qq.com>
2017-11-22 11:27:11 +0800

    change chown for docker

--- docker-build.sh
+++ docker-build.sh
@@ -1,1 +1,2 @@
 docker build -t dean/jenkins .
+
--- docker-run.sh
+++ docker-run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+sudo chown -R 1000:1000 /usr/local/jenkins_home
 docker run \
-	-d \
 	--name "jenkins" \
+	-d \
 	-p 8080:8080 \
-	-v ~/usr/local/jenkins:/var/jenkins_home \
+	-v /usr/local/jenkins_home:/var/jenkins_home \
 	dean/jenkins:latest
 
 	#-v ~/Users/deanguo/jenkins:/var/jenkins_home \
